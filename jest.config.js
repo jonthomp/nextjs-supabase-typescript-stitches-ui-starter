a1fdf91920bf8a9f5adeedab6a5fad314ea14d43
Jon Thompson <jon@jont.me>
2021-07-29 22:24:37 +0100

    Adds tests setup (jest, react-testing-library, cypress)

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+const config = {
+  verbose: true,
+  testRegex: "((\\.|/*.)(spec|test))\\.tsx?$",
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/cypress/",
+    "<rootDir>/node_modules/",
+  ],
+
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.css$": "<rootDir>/__mocks__/cssTransform.js",
+  },
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect",
+    "jest-extended",
+    "<rootDir>/jest.setup.ts",
+  ],
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
+  moduleNameMapper: {
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  globals: {
+    window: {},
+    "ts-jest": {
+      tsConfig: "<rootDir>/tsconfig.jest.json",
+    },
+  },
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+};
+
+module.exports = config;
